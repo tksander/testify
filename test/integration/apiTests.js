4e5ab45fc0e41141eab7c260dfe11ad3400869c3
Brett Sanders <bsand08@gmail.com>
2015-10-03 16:30:59 -0700

    Add integration tests

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,39 @@
+
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  beforeEach(function() {
+    JSONresponse = {
+        "todos": [
+          {
+            "name": "Finish unit tests",
+            "done": true
+          },
+          {
+            "name": "Finish integration tests",
+            "done": false
+          },
+          {
+            "name": "Finish end-to-end tests",
+            "done": false
+          }
+        ]
+      };
+    server = sinon.fakeServer.create();
+    server.respondWith("GET", "http://localhost:3000/todos",
+                       [200, { "Content-Type": "application/json" },
+                        JSON.stringify(JSONresponse)]);
+    setupStub = sinon.stub(todo, 'setup');
+  });
+
+  afterEach(function() {
+    server.restore();
+    setupStub.restore();
+  });
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
+    todo.init();
+    server.respond();
+    expect(setupStub.calledWith(JSONresponse.todos)).to.be.true;
   });
 });
